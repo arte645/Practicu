1ba5aeb0a1f2579be1cf0a5a4591c4e2a71f433a
Artyom <ovs.artm@gmail.com>
2024-12-06 22:16:49 +0600

    add classes to tests

--- lab2/tests/test_getAuthToken.py
+++ lab2/tests/test_getAuthToken.py
@@ -1,9 +1,10 @@
 import lab2.loadJson as loadJson
 from lab2.Api.Api import Api
 
-def test_getAuthToken():
-    api = Api()
-    loginPassword = loadJson.load_json('../jsons/admin.json')
-    schema = loadJson.load_json('../schemas/AuthResponseSchema.json')
-    api.post('auth', loginPassword).checkout(schema=schema)
+class TestGetAuthToken:
+    def test_getAuthToken(self):
+        api = Api()
+        loginPassword = loadJson.load_json('../jsons/admin.json')
+        schema = loadJson.load_json('../schemas/AuthResponseSchema.json')
+        api.post('auth', loginPassword).checkout(schema=schema)
     
--- lab2/tests/test_getBookingById.py
+++ lab2/tests/test_getBookingById.py
@@ -1,8 +1,9 @@
 import lab2.loadJson as loadJson
 from lab2.Api.Api import Api
 
-def test_getBookingById():
-    api = Api()
-    schema = loadJson.load_json('../schemas/GetBookingResponseSchema.json')
-    api.get('booking/1').checkout(schema=schema)
+class TestGetBookingById:
+    def test_getBookingById(self):
+        api = Api()
+        schema = loadJson.load_json('../schemas/GetBookingResponseSchema.json')
+        api.get('booking/1').checkout(schema=schema)
     
